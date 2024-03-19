dca6c2d530d423455d715ec088b87ef730dec86b
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-03-19 16:46:43 -0700

    feat: :tada:

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const DB_TASKS = require('./tasks');
+const cors = require('cors');
+const PORT = 3000;
+
+const tasks = [...DB_TASKS];
+
+const app = express();
+// app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cors());
+
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
+}); 
+
+app.get('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+  let selectedTask = tasks.find(task => task.id == taskId);
+  if(!selectedTask){ 
+    res.statusCode = 404;
+    res.send(`ID ${taskId} not found`);
+  }
+  res.send(selectedTask);
+  console.log(selectedTask);
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = req.body;
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+})
+
+app.delete('/tasks/:id', (req, res) => {
+  const deleteTaskId = req.params.id;
+  const deleteTask = tasks.find(task => task.id == deleteTaskId);
+  const deleteTaskPosition = tasks.indexOf(deleteTask);
+  tasks.splice(deleteTaskPosition, 1);
+  res.status(201).json(deleteTaskId);
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  const patchTaskId = req.params.id;
+  const patchTaskBody = req.body.task;
+  const patchTask = tasks.find(task => task.id == patchTaskId);
+  patchTask.task = patchTaskBody
+  res.send(patchTask);
+})
+
+app.put('/tasks/:id', (req, res) => {
+  const patchTaskId = req.params.id;
+  const patchTaskBody = req.body.task;
+  const patchTask = tasks.find(task => task.id == patchTaskId);
+  patchTask.task = patchTaskBody
+  res.send(patchTask);
+})
+
+app.listen(PORT, () => {
+    console.log(`App escuchando en el puerto ${PORT}`)
+})
---
+++ tasks.js
@@ -0,0 +1,19 @@
+let tasks = [
+  {
+    id: 1,
+    task: "test",
+    status: "not started"
+  },
+  {
+    id: 2,
+    task: "test 2",
+    status: "in progress"
+  },
+  {
+    id: 3,
+    task: "test 3",
+    status: "done"
+  },
+]
+
+module.exports = tasks
